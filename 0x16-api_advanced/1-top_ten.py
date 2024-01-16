d81f46342a775e5d562b1e91898639ec22ac4695
jassyio <joejassyio@gmail.com>
2024-01-16 22:12:55 +0300

    new

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,42 +5,20 @@ top ten hot posts of a subreddit
 """
 from requests import get
 
+
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
         return
-
-    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    params = {'limit': 10}
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
-
-    response = get(url, headers=user_agent, params=params)
-
-    # Check if the request was successful (HTTP status code 200)
-    if response.status_code != 200:
-        print("None")
-        return
-
-    try:
-        results = response.json()
-        my_data = results['data']['children']
-
-        for post in my_data:
-            print(post['data']['title'])
-
-    except Exception as e:
-        print(f"Error: {e}")
-        print("None")
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
